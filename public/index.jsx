2fe58bb7784b69324bd02fe77750dba1d6d7c221
Lewis Nyabuto <nyabutolewis33@gmail.com>
2024-11-30 12:35:13 +0300

    firstcommit

---
+++ public/index.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+root.render(<h1>hello, React!</h1>)
+const root = createRoot(document.getElementById("root1"))
+root1.render(<p>the next best thing</p>)
